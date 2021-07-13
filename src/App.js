c6fad70b7954b2cbe0c5836c1d279dfdb243aa43
Mateus V. Farias <mateus_vinicius15@outlook.com>
2021-07-13 00:48:31 -0300

    feat: enable automatic runtime in react

--- src/App.js
+++ src/App.js
@@ -1,2 +1,3 @@
-import React from 'react';
-import { App } from './index';
+export function App() {
+  return <h1>Hello World</h1>;
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import React from 'react';
+import { render } from 'react-dom';
+import { App } from './App';
 
-export function App() {
-  return <h1>Hello World</h1>;
-}
+render(<App />, document.getElementById('root'));
