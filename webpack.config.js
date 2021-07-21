a5c49f4990f11f5e9f7c6f6a251cb97c64951b9a
Mateus V. Farias <mateus_vinicius15@outlook.com>
2021-07-21 04:19:57 -0300

    chore: add typescript support to a babel react

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,13 @@ const isDevelopment = process.env.NODE.ENV !== 'production';
 module.exports = {
   mode: isDevelopment ? 'development' : 'production',
   devtool: isDevelopment ? 'eval-source-map' : 'source-map',
-  entry: path.resolve(__dirname, 'src', 'index.js'),
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'public', 'index.html'),
@@ -28,7 +28,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(j|t)sx$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
