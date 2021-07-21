8f29b31e8669cdae751132b18bc6d4ec0a53c042
Mateus V. Farias <mateus_vinicius15@outlook.com>
2021-07-21 04:22:10 -0300

    feat: add hover effect

--- src/App.js
+++
@@ -1,5 +0,0 @@
-import './styles/global.scss';
-
-export function App() {
-  return <h1>Hello World</h1>;
-}
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import './styles/global.scss';
+
+export function App() {
+  return <h1>React App</h1>;
+}
