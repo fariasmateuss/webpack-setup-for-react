8dbd19935b5f2a25ea85c1b84735fae05c28122e
Mateus V. Farias <mateus_vinicius15@outlook.com>
2021-11-23 23:36:06 -0400

    chore: add setting up import SVG as a URL

--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,17 @@ module.exports = {
         exclude: /node_modules/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
     ],
   },
 };
