8dedcd547baa8482f2332e58c861b90ca8f8d417
Mateus V. Farias <mateus_vinicius15@outlook.com>
2021-11-23 23:41:46 -0400

    refactor: add new concept to landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
 import './styles/global.scss';
+import './styles/App.scss';
+
+import logo from './assets/logo.svg';
 
 export function App() {
-  return <h1>React App</h1>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          To get started, fork it and then edit <span>src/App.tsx</span> and
+          save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
